a4ef6ca07bdd8550abb5b377bf7ca4febd6290e7
Edwin Kcomt <ekcomtp@gmail.com>
2024-10-05 10:58:06 -0500

    Examen Auto Movil

---
+++ qa-lab-mobile-automation/src/test/java/com/nttdata/screens/GalleryScreen.java
@@ -0,0 +1,13 @@
+package com.nttdata.screens;
+
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.WebElement;
+
+public class GalleryScreen extends PageObject {
+
+    private WebElement backpackProduct;
+    private WebElement boltProduct;
+    private WebElement bike;
+
+
+}
